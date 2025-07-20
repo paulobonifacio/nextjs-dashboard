de28d5ee5f1f94916ab975fd94e9cf16b5f2baab
paulobonifacio <paulo.bonifacio@live.com>
2025-07-20 18:42:36 +0200

    W3 fix

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+import { inter } from '@/app/ui/fonts';
+import '@/app/ui/global.css';
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+    <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
